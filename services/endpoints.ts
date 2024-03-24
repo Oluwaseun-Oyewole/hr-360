dfdfa733cbf12ac03e19fbba4019090aafb068ee
Oluwaseun Oyewole <findseunoyewole@gmail.com>
2024-03-24 11:00:38 +0100

    fix base url

--- services/endpoints.ts
+++ services/endpoints.ts
@@ -1,9 +1,9 @@
-export const BASE_API_URL = process.env.BASE_API_URL;
+export const BASE_URL = process.env.BASE_API_URL + "/api/";
 
 export class Endpoints {
-  static register = BASE_API_URL + "/register";
-  static forgotPassword = BASE_API_URL + "/forgotPassword";
-  static resetPassword = BASE_API_URL + "/resetPassword";
-  static changeEmail = BASE_API_URL + "/email";
-  static updateAccount = BASE_API_URL + "/accountUpdate";
+  static register = BASE_URL + "/register";
+  static forgotPassword = BASE_URL + "/forgotPassword";
+  static resetPassword = BASE_URL + "/resetPassword";
+  static changeEmail = BASE_URL + "/email";
+  static updateAccount = BASE_URL + "/accountUpdate";
 }
